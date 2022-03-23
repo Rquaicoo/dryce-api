7a5eeaa324086772f859b4949798ff357000796d
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-03-23 23:21:30 +0000

    authentication APIs added

--- dryce/api/models.py
+++ dryce/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class RegularUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL)
+    phone_number = models.CharField(max_length=10, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.user.username)
