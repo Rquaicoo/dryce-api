20275dd90982c7c8b7b12497856f9a87ec86ae66
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-04-19 23:00:52 +0000

    path for resending otp added

--- dryce/api/url.py
+++ dryce/api/url.py
@@ -1,10 +1,12 @@
 from django import views
 from django.urls import path
 from .views import *
+
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('auth/register/', CreateUserView.as_view(), name="register"),
     path('auth/login/', obtain_auth_token, name="login"),
     path('auth/logout/', LogoutUserAPIView.as_view(), name="logout"),
-    path('auth/verify_user/', VerifyUserAPIView.as_view(), name="verify_user")
+    path('auth/verify_user/', VerifyUserAPIView.as_view(), name="verify_user"),
+    path('auth/vendor/register/', CreateUserView.as_view(), name="vendor_register"),
 ]
