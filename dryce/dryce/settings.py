7d56f559eab60238ab07370535b613c6017228cf
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-05-21 21:07:59 +0000

    new migrations made

--- dryce/dryce/settings.py
+++ dryce/dryce/settings.py
@@ -98,12 +98,37 @@ WSGI_APPLICATION = 'dryce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dryce',
+        'USER': 'postgres',
+        'PASSWORD': 'R0245437556q',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd1qmsmgeorld85',
+        'USER': 'rbhqzrsijwiabq',
+        'PASSWORD': '45ec37ee75d5c622807a7f71f038ff3770042477f6de144c11db88bbff2a33ff',
+        'HOST': 'ec2-54-160-109-68.compute-1.amazonaws.com',
+        'PORT': '5432',
+    },
+}
+
 
 
 # Password validation
--- dryce/vendor/models.py
+++ dryce/vendor/models.py
@@ -17,14 +17,18 @@ class Vendor(models.Model):
 class VendorDetails(models.Model):
     vendor = models.OneToOneField(Vendor, on_delete=models.CASCADE, blank=False)
 
+    business_name=models.CharField(max_length=100, blank=False, null=False) 
+    phone_number=models.CharField(max_length=10, blank=False, null=False)
+    address = models.CharField(max_length=100, blank=False, null=False)
+    email = models.EmailField(blank=True, null=True)
+    city = models.CharField(max_length=100, blank=False, null=False)
+    region = models.CharField(max_length=100, blank=False, null=False)
     business_description = models.CharField(max_length=100, blank=False, null=False)
     business_certificate=models.FileField(blank=False)
-    resume=models.FileField(null=False,blank=False)
-    business_name=models.CharField(max_length=100, blank=False, null=False) 
-    phone_number=models.CharField(max_length=10, blank=False, null=False)  
+     
     business_picture=models.ImageField(blank=True)
+    business_logo=models.ImageField(blank=True)
 
-    location=models.CharField(max_length=100, null=False,blank=False)
     opening_hours=models.CharField(max_length=100, blank=True, null=True)
     closing_hours=models.CharField(max_length=100, blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
